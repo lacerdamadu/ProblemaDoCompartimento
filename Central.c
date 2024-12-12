968a466f27b5b21679762b7a34fd18ec2fa721cb
Ana Clara <nanakkj.sillaby@gmail.com>
2024-12-12 20:44:20 -0300

    strg

--- Central.c
+++ Central.c
@@ -52,10 +52,12 @@ int EntradaPorArquivo(TSondas *ListaSondas){
         elementos[j] = novarocha.id;
     }
     todas_combinacoes(tamanho);
+
+    return 0;
 }
 
 void combinacao_simples(int n, int r, int x[], int next, int k, int **matrizindices, int *index){
-    printf("oi");
+    
     if (k == r) {
     for (int i = 0; i < r; i++) {
       matrizindices[*index][i] = x[i];
@@ -71,7 +73,7 @@ void combinacao_simples(int n, int r, int x[], int next, int k, int **matrizindi
 }
 
 void todas_combinacoes(int n){
-    printf("oik");
+    
   int total_matrizindices = (1 << n) - 1;
   int **matrizindices = (int **)malloc(total_matrizindices * sizeof(int *));
 
@@ -96,91 +98,3 @@ void todas_combinacoes(int n){
     printf("\n");
   }
 }
-
-/*void Redistribuicao(TSondas *Sondas, Sonda *temporaria, int tamanho, int elementos[]){
-    printf("oi");
-    int linhas = 0, *x;
-    x = malloc((tamanho+1) * sizeof(int));
-    linhas = CalculaNumCombinacoes(tamanho);
-    printf("%d", linhas);
-    
-    int** matind = (int**) malloc(sizeof(int*) * linhas);
-    for(int i=0; i <= tamanho; i++){
-        matind[i] = (int*) malloc(sizeof(int) * tamanho+1);
-    }
-    memset(matind, -1, sizeof(matind));
-    for(int j=0; j<linhas; j++){
-        for(int k=0;k<tamanho; k++){
-            printf("%d", *matind[k]);
-        }
-        printf("\n");
-    }
-}
-/*
-    for (int k = 0; k <= tamanho; k++){
-        combinacao_simples(tamanho, k, x, 0, 0, matind);
-    }
-}
-
-int pos = 0;
-void combinacao_simples(int tamanho, int r, int x[], int next, int k, int **matind){
-    int i;
-    if (k == r){
-        for (i = 0; i < r; i++){
-            matind[pos][i] = x[i];
-            printf("%d", matind[pos][i]);
-        }
-        pos++;
-    }
-    else{
-        for (i = next; i < tamanho; i++){
-            x[k] = i;
-            combinacao_simples(tamanho, r, x, i + 1, k + 1, matind);
-        }
-    }
-}
-
-int Fatorial(int tamanho){
-    int res = 1;
-    for (int i = 1; i <= tamanho; i++){
-        res *= i;
-    }
-    return res;
-}
-int Combinacao(int tamanho, int k){
-    if (k > tamanho)
-        return 0;
-    return (int)(Fatorial(tamanho) / (Fatorial(k) * Fatorial(tamanho - k)));
-}
-
-int CalculaNumCombinacoes(int tamanho){
-    int sum = 0;
-    for (int k = 1; k <= tamanho; k++)
-    {
-        sum += Combinacao(tamanho, k);
-    }
-    return sum;
-}
-
-
-
-/* int usados[] = [0,0,0,0,0,0,0];
-for(int i=0; i<3; i++){
-}
-    melhor_comb = []
-    melhor_valor = -1
-    for comb in  [[0,7], [5,8]]:
-        for rocha in comb:
-            if usados[rocha] :
-                comb_invalida = True 
-        
-        valor = val(comb)
-        peso = peso(comb)
-
-        if peso < capa and valor > melhor_valor and not comb_invalida:
-            melhor_valor = valor
-            melhor_comb = comb
-    
-    for rocha in melhor_comb:
-        usados[rocha] = 1*/
-
--- Central.h
+++ Central.h
@@ -11,6 +11,6 @@ void Redistribuicao(TSondas *Sondas, Sonda *temporaria, int tamanho, int element
 int Combinacao(int n, int k);
 int CalculaNumCombinacoes(int n);*/
 void todas_combinacoes(int n);
-void combinacao_simples(int n, int r, int x[], int next, int k, int **matrizindices, int *index)
+void combinacao_simples(int n, int r, int x[], int next, int k, int **matrizindices, int *index);
 
 #endif
