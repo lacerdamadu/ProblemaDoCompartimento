12963dc5768dcef0f6cffe1db3774c7941538eda
Ana Clara <nanakkj.sillaby@gmail.com>
2024-12-13 03:25:15 -0300

    finalzin

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <time.h>
 #include "Central.h"
+#define TAMANHOMAX 1000000000
 
 int main(){
     TSondas Sondas;
     EntradaPorArquivo(&Sondas);
-    clock_t start, end;
-    double cpu_time_used;
+    clock_t comeco, final;
+    double tempofinal;
 
-    start = clock();
-    // Código que você quer medir o tempo de execução
-    for (long i = 0; i < 1000000000; i++);
-    end = clock();
+    comeco = clock();
+    for (long i = 0; i < TAMANHOMAX; i++);
+    final = clock();
 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Tempo de execucao: %.2f segundos\n", cpu_time_used);
+    tempofinal = ((double) (final - comeco)) / CLOCKS_PER_SEC;
+    printf("Tempo gasto: %.2f segundos\n", tempofinal);
 
     return 0;
 }
